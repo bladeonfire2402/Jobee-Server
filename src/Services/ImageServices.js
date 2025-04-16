45a9bc634c6f19322f21081f58df7074f084cbcc
bladeonfire2402 <liemp966@gmail.com>
2025-04-16 23:21:51 +0700

    install cloundinary

---
+++ src/Services/ImageServices.js
@@ -0,0 +1,12 @@
+class ImageServices{
+    createCloundaryUploadJob=async(buffer,id)=>{
+        
+
+    }
+
+    createCloundaryUploadEmployer=async(buffer,userId)=>{
+
+    }
+
+}
+export default new ImageServices
